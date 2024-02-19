fa009c836bc0f987a5d27684e9f9aba3a0f78a66
Durga Mane <83406616+durgamane12@users.noreply.github.com>
2024-02-19 11:53:01 +0530

    Add files via upload

---
+++ Class/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        int num1;
+        int num2;
+        int result;
+
+        void Add()
+        {
+            result = num1 + num2;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        void Sub()
+        {
+            result = num1 - num2;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        void Mul()
+        {
+            result = num1 * num2;
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+
+        public static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+
+            cal.num1 = 40;
+            cal.num2 = 20;
+
+            cal.Add();
+            cal.Sub();
+            cal.Mul();
+
+            Calculator cal1 = new Calculator();
+
+            cal1.num1 = 100;
+            cal1.num2 = 40;
+
+            cal1.Add();
+            cal1.Sub();
+            cal1.Mul();
+
+        }
+
+
+    }
+}
